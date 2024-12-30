670f318fc615256445eb91a46cc62cd8fd2b059b
Maiko Duarte <mkdu185@gmail.com>
2024-12-30 16:02:43 -0300

    Service layer, component registration

---
+++ src/main/java/com/maikoduarte/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.maikoduarte.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.maikoduarte.course.entities.User;
+import com.maikoduarte.course.repositories.UserRepository;
+
+@Service
+public class UserService {
+
+  @Autowired
+  private UserRepository repository;
+
+  public List<User> findAll() {
+    return repository.findAll();
+  }
+
+  public User findById(Long id) {
+    Optional<User> obj = repository.findById(id);
+    return obj.get();
+  }
+  
+}
